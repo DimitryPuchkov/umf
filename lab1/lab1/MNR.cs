0e0d47b6239523475531fa87ef667aedc6fcfd7d
Dmitry <puchkovdmitrybor@gmail.com>
2023-02-10 08:22:07 +0700

    add lab1; add class MNR and read files

---
+++ lab1/lab1/MNR.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+   class MNR
+   {
+      private int NumVertices { get; set; }
+      private List<List<double>> Vertices { get; set; }
+      private List<double> GridX { get; set; }
+      private List<double> GridY { get; set; }
+      public MNR(string vertices_path, string grid_path)
+      {
+         using (StreamReader reader = new StreamReader(vertices_path))
+         {
+            this.NumVertices = int.Parse(reader.ReadLine());
+            Vertices = new List<List<double>>();
+            for(int i = 0; i<this.NumVertices; i++)
+            {
+               var line = reader.ReadLine();
+               var coords = line?.Split(' ')?.Select(double.Parse)?.ToList();
+               Vertices.Add(coords);
+            }
+         }
+         using (StreamReader reader = new StreamReader(grid_path))
+         {
+            var line = reader.ReadLine();
+            GridX = new List<double>();
+            GridY = new List<double>();
+            GridX = line?.Split(' ')?.Select(double.Parse)?.ToList();
+            line = reader.ReadLine();
+            GridY = line?.Split(' ')?.Select(double.Parse)?.ToList();
+         }
+      }
+
+   }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab1
+{
+   class Program
+   {
+      
+      static void Main(string[] args)
+      {
+         MNR mnr = new MNR("vertices.txt", "grid.txt");
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
